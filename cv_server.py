9527c2a7c22a896a52c0f0a258cfab63192f0aa4
Mitch Francis <vaektor.phresh@gmail.com>
2020-08-09 10:15:54 -0700

    Add README. Adjust resolution

--- cv_server.py
+++ cv_server.py
@@ -17,7 +17,7 @@ camera = cv2.VideoCapture(0)  # init the camera
 while True:
     try:
         grabbed, frame = camera.read()  # grab the current frame
-        frame = cv2.resize(frame, (640, 480))  # resize the frame
+        frame = cv2.resize(frame, (1200, 1600))  # resize the frame
         encoded, buffer = cv2.imencode('.jpg', frame)
         jpg_as_text = base64.b64encode(buffer)
         footage_socket.send(jpg_as_text)
